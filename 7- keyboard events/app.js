6152af4f659cd9150e6a77737d9272c2c9e41124
RaminAfhami993 <raminafhami.ra@yahoo.com>
2020-07-13 19:49:32 +0430

    keyboard events and modifires

---
+++ 7- keyboard events/app.js
@@ -0,0 +1,19 @@
+new Vue({
+    el: "#mainApp",
+    data: {
+    },
+    methods: {
+        logname: function() {
+            console.log("name");
+            
+        }, 
+        logage: function() {
+            console.log("age");
+            
+        },
+        logdesc: function() {
+            console.log("desc");
+            
+        }
+    }
+});
