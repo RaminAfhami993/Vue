5e68ccb7630bf593244a8eeb29e0e27c51878cf1
RaminAfhami993 <raminafhami.ra@yahoo.com>
2020-07-15 06:12:08 +0430

    loop

---
+++ 93- loop/app.js
@@ -0,0 +1,22 @@
+new Vue({
+    el: "#mainApp",
+    data: {
+        drinks: ["milk", 'coffee', 'chai'],
+        colors: [{
+            name: "black",
+            numbers: 25
+        }, {
+            name: "red",
+            numbers: 100
+        }, {
+            name: "pink",
+            numbers:50
+        }]
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+});
