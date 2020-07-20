68b24a4a9e4786d8df3a32de54f9a1bed0b4522d
RaminAfhami993 <afhami.ramin@yahoo.com>
2020-07-20 09:29:34 +0430

    first app

---
+++ 99- first app vue cli/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <h1> {{ title }} </h1>
+    <p> {{ greeting() }} </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      title: "VUE CLI"
+    }
+  },
+  methods: {
+    greeting() {
+      return `hello from ${this.title}`
+    }
+   },
+}
+</script>
+
+<style>
+
+</style>
