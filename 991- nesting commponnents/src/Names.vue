282fa1ffc35cdc12f762ecbc3b6995cb89fe2aba
RaminAfhami993 <afhami.ramin@yahoo.com>
2020-07-20 10:18:52 +0430

    global and local nesting components

---
+++ 991- nesting commponnents/src/Names.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <ul>
+        <li v-for="name in names"> {{ name }} </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+        names: ['ramin', 'mahone', 'bahram']
+    }
+  }
+}
+</script>
+
