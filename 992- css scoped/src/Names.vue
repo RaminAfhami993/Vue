6a7e7c9f9d404acf3c66b3ad8a9e5d6425e2d8d5
RaminAfhami993 <afhami.ramin@yahoo.com>
2020-07-21 10:18:03 +0430

    css scope

---
+++ 992- css scoped/src/Names.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>inner h1</h1>
+    <ul>
+        <li v-for="name in names"> {{ name }} </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+        names: ['ramin', 'mahone', 'bahram']
+    }
+  }
+}
+</script>
+
+
+<style>
+  h1 {
+    color: purple
+  }
+</style>
+
