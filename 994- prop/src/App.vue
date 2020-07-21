a9d2fde5dd0af5c22debee57fb2d375bab3034c6
RaminAfhami993 <afhami.ramin@yahoo.com>
2020-07-21 12:09:01 +0430

    prop

---
+++ 994- prop/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <app-header></app-header>
+    <app-body :names="names"></app-body>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import Header from './components/header.vue'
+import Footer from './components/footer.vue'
+import Names from './components/names.vue'
+
+export default {
+  components: {
+    'app-header': Header,
+    'app-footer': Footer,
+    'app-body': Names
+  },
+  data () {
+    return {
+        names: [
+            {name: 'Ramin', technology: 'Node', show: false},
+            {name: 'Mahone', technology: 'CSS', show: false},
+            {name: 'Pedram', technology: 'JS', show: false},
+            {name: 'Arsham', technology: 'HTML', show: false}
+        ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
