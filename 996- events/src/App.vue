c30844a90f4fcb2f5fb09d26c3d6b2e08fd8207b
RaminAfhami993 <afhami.ramin@yahoo.com>
2020-07-22 12:14:54 +0430

    events

---
+++ 996- events/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <app-header></app-header>
+    <app-body :names="names" @firstEvent="changeTitle($event)" :title="title"></app-body>
+    <hr />
+    <app-body :names="names" @firstEvent="changeTitle($event)" :title="title"></app-body>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import Header from './components/header.vue'
+import Footer from './components/footer.vue'
+import Names from './components/names.vue'
+
+export default {
+  components: {
+    'app-header': Header,
+    'app-footer': Footer,
+    'app-body': Names
+  },
+  data () {
+    return {
+        names: [
+            {name: 'Ramin', technology: 'Node', show: false},
+            {name: 'Mahone', technology: 'CSS', show: false},
+            {name: 'Pedram', technology: 'JS', show: false},
+            {name: 'Arsham', technology: 'HTML', show: false}
+        ],
+        title: "this is a test string"
+    }
+  },
+  methods: {
+    changeTitle(c) {
+            console.log(c)
+            let x = this.title.split('');
+            x.pop();
+            this.title = x.join('');
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ 996- events/src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+    <footer>
+                <p>Copyright 2017 {{ title }}</p>
+    </footer>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'example'
+    }
+  }
+}
+</script>
+
+<style scoped>
+    footer {
+        background: #222;
+        padding: 6px;
+    }
+    p {
+        color: lightgreen;
+        text-align: center;
+    }
+</style>
