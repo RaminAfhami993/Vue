39bf6de809eeb9ddc0706ec397a01dd50b8646e8
RaminAfhami993 <afhami.ramin@yahoo.com>
2020-07-23 16:08:07 +0430

    slots

---
+++ 999- slot/src/components/FormHelper.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <!--
+        <slot></slot>
+        <h1>
+            Form Helper main text
+        </h1>
+        <slot></slot>
+        -->
+
+        <slot name="before"></slot>
+        <h1>
+            Form Helper main text
+        </h1>
+        <slot name="after"></slot>
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  color: green
+}
+</style>
+
