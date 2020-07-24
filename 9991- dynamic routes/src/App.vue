5c537d29ebf1de48f46d688796ef8925e520d5b7
RaminAfhami993 <afhami.ramin@yahoo.com>
2020-07-24 10:02:04 +0430

    dynamic components

---
+++ 9991- dynamic routes/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <keep-alive>
+        <component :is="component"></component>
+    </keep-alive>
+    <button @click="component = 'loginForm'">show loginForm</button>
+    <button @click="component = 'messageForm'">show messageForm</button>
+  </div>
+</template>
+
+<script>
+// Imports
+import LoginForm from "./components/loginForm.vue"
+import MessageForm from "./components/messageForm.vue"
+
+export default {
+  components: {
+    "loginForm": LoginForm,
+    "messageForm": MessageForm
+  },
+  data () {
+    return {
+      component: "loginForm"
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
