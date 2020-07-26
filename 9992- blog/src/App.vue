32d489638259d7e1e9459f4576514d88a4bc33b5
RaminAfhami993 <afhami.ramin@yahoo.com>
2020-07-26 16:41:10 +0430

    show blogs

--- 9992- blog/src/App.vue
+++ 9992- blog/src/App.vue
@@ -1,16 +1,27 @@
 <template>
-    <add-blog></add-blog>
+  <div> 
+    <keep-alive>
+        <component :is="component"></component>
+    </keep-alive>
+
+    <button @click="component = 'add-blog'"> add new blog </button>
+    <button @click="component = 'all-blogs'"> all blogs </button>
+  </div> 
 </template>
 
 <script>
 
-import AddBlog from './components/AddBlog.vue'
+import AddBlog from './components/AddBlog.vue';
+import AllBlogs from './components/AllBlogs.vue';
+
 export default {
   components: {
-    "add-blog": AddBlog
+    "add-blog": AddBlog,
+    "all-blogs": AllBlogs
   },
   data () {
     return {
+      component: 'all-blogs'
     }
   }
 }
