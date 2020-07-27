40fb516ae13bca0f6f72d06546f35c9614e5f5e5
RaminAfhami993 <afhami.ramin@yahoo.com>
2020-07-27 14:54:22 +0430

    custom directive

--- 9992- blog/src/components/AllBlogs.vue
+++ 9992- blog/src/components/AllBlogs.vue
@@ -1,8 +1,8 @@
 <template>
-    <div id="show-blogs">
+    <div id="show-blogs" v-theme:column="'narrow'">
         <h1>All Blog Articles</h1>
-        <div v-for="blog in blogs" class="single-blog">
-            <h2>{{ blog.title }}</h2>
+        <div v-for="blog in blogs" class="single-blog" >
+            <h2 v-rainbow>{{ blog.title }}</h2>
             <article>{{ blog.body }}</article>
         </div>
     </div>
--- 9992- blog/src/main.js
+++ 9992- blog/src/main.js
@@ -4,6 +4,27 @@ import VueResource from 'vue-resource'
 
 Vue.use(VueResource);
 
+Vue.directive('rainbow', {
+  bind(element, binding, vnode) {
+    element.style.color = '#' + Math.random().toString().slice(2, 8)
+  }
+});
+
+Vue.directive('theme', {
+  bind(el, binding, vnode) {
+    if (binding.value === "wide") {
+      el.style.maxWidth = "1000px";
+    } else if (binding.value === "narrow") {
+      el.style.maxWidth = "560px";
+    };
+
+    if (binding.arg === 'column') {
+      el.style.padding = "20px";
+      el.style.background ="#ddd"
+    }
+  }
+})
+
 new Vue({
   el: '#app',
   render: h => h(App)
